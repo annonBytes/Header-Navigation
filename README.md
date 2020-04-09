82d62c23ddf5af4c50f67328c37c8eb2c69bf130
annonBytes <beinmonyudaniel@gmail.com>
2020-04-09 14:36:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-wa1k1m
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-wa1k1m)
---
+++ index.js
@@ -0,0 +1,11 @@
+// Import stylesheets
+import './style.css';
+
+const burger = document.getElementById('burger');
+const ul = document.querySelector('nav ul');
+
+burger.addEventListener('click', () => {
+
+ul.classList.toggle('show');
+  
+});
